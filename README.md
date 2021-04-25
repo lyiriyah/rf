cfcf23c34e3e8a15dbc2811e0f1117922b429f33
lyiriyah <oscar.wittamsnangle@gmail.com>
2021-04-25 19:03:47 +0100

    Segment information extraction

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Clone and run `cargo install --path .` to compile and install rf.
 Make sure to have `$HOME/.cargo/bin` in your path (or install to another directory with `cargo install --path . --root <install-dir>`).
 
 ## Todo
-- [ ] Segment out getting information into functions
+- [x] Segment out getting information into functions
 - [ ] Put onto crates.io
--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,21 @@ use colored::*;
 use regex::Regex;
 use sys_info;
 
-fn main() {
+fn get_cpu_info() -> String {
     let re = Regex::new(" +").unwrap();
     let cpuid = raw_cpuid::CpuId::new();
-    let hostname = whoami::hostname();
-    let username = whoami::username();
     let cpuextinf = cpuid.get_extended_function_info();
-    let cpuinfo = re.replace_all(cpuextinf.as_ref().map_or_else(
+    
+    return re.replace_all(cpuextinf.as_ref().map_or_else(
             || "n/a",
             |extfuninfo| extfuninfo.processor_brand_string().unwrap_or("unreadable"),
-        ), " ");
-    
-    let mut editor = "";
-    let mut editorpath = String::new();
-    let mut shell = String::new();
+        ), " ").to_string();
+}
 
+fn get_editor() -> String {
+    let mut editor = String::from("");
+    let mut editorpath = String::new();
+    
     match env::var("EDITOR") {
         Ok(path) => editorpath = path,
         Err(e) => println!("Failed to get editor with error {}", e)
@@ -29,11 +29,17 @@ fn main() {
 
     match Path::new(&editorpath).file_name() {
         Some(path) => match path.to_str() {
-            Some(basename) => editor = basename,
+            Some(basename) => editor = basename.to_string(),
             None => println!("Failed to convert the basename of {} to a &str", editorpath)
         },
         None => println!("Could not get basename of editor {}", editor)
     }
+
+    return editor;
+}
+
+fn get_shell() -> String {
+    let mut shell = String::new();
 
     match Passwd::from_name(&username) {
         Some(user) => match Path::new(&user.shell).file_name() {
@@ -46,10 +52,24 @@ fn main() {
         None => println!("Failed to get shell for user {}", username)
     }
 
-    let meminfo = match sys_info::mem_info() {
+    return shell;
+}
+
+fn get_meminfo() -> MemInfo {
+    return match sys_info::mem_info() {
         Ok(meminfo) => meminfo,
         Err(e) => std::panic!("Failed to get memory info with error {}", e)
     };
+}
+
+fn main() {
+    let hostname = whoami::hostname();
+    let username = whoami::username();
+
+    let cpuinfo = get_cpu_info();
+    let editor = get_editor();
+    let shell = get_shell();
+    let meminfo = get_meminfo();
 
     let mut topline = String::from("");
 
